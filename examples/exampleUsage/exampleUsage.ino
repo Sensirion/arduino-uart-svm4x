c53c4cf9da136ee40ffaa3e7cbb0570146c3e531
Bjoern Muntwyler <Bjoern.Muntwyler@sensirion.com>
2021-07-16 12:02:22 +0200

    Add example usage

--- examples/exampleUsage/exampleUsage.ino
+++ examples/exampleUsage/exampleUsage.ino
@@ -1,9 +1,4 @@
 /*
- * SHDLC-Generator: 0.8.2
- * Yaml Version: 0.4.0
- * Template Version: 0.7.0-20-gf035cde
- */
-/*
  * Copyright (c) 2021, Sensirion AG
  * All rights reserved.
  *
@@ -37,13 +32,11 @@
 #include <Arduino.h>
 #include <SensirionUartSvm41.h>
 
-// Adjust as needed for you Arduino board
-#define SENSOR_SERIAL_INTERFACE \
-    Serial  // TODO: DRIVER_GENERATOR adjust to Serial1
+// Adjust as needed for you Arduino board.
+// [Serial, Serial1, Serial2, etc.]
+#define SENSOR_SERIAL_INTERFACE Serial1
 
 SensirionUartSvm41 svm41;
-
-// TODO: DRIVER_GENERATOR Add missing commands and make prints more pretty
 
 void setup() {
     uint16_t error;
@@ -61,11 +54,19 @@ void setup() {
 
     svm41.begin(SENSOR_SERIAL_INTERFACE);
 
-    unsigned char serialNumber[255];
-    uint8_t serialNumberSize = 255;
+    error = svm41.deviceReset();
+    if (error) {
+        Serial.print("Error trying to execute deviceReset(): ");
+        errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+    }
 
+    // Delay to let the serial monitor catch up
+    delay(2000);
+
+    uint8_t serialNumber[32];
+    uint8_t serialNumberSize = 32;
     error = svm41.getSerialNumber(serialNumber, serialNumberSize);
-
     if (error) {
         Serial.print("Error trying to execute getSerialNumber(): ");
         errorToString(error, errorMessage, 256);
@@ -75,34 +76,6 @@ void setup() {
         Serial.println((char*)serialNumber);
     }
 
-    unsigned char productType[255];
-    uint8_t productTypeSize = 255;
-
-    error = svm41.getProductType(productType, productTypeSize);
-
-    if (error) {
-        Serial.print("Error trying to execute getProductType(): ");
-        errorToString(error, errorMessage, 256);
-        Serial.println(errorMessage);
-    } else {
-        Serial.print("ProductType:");
-        Serial.println((char*)productType);
-    }
-
-    unsigned char productName[255];
-    uint8_t productNameSize = 255;
-
-    error = svm41.getProductName(productName, productNameSize);
-
-    if (error) {
-        Serial.print("Error trying to execute getProductName(): ");
-        errorToString(error, errorMessage, 256);
-        Serial.println(errorMessage);
-    } else {
-        Serial.print("ProductName:");
-        Serial.println((char*)productName);
-    }
-
     uint8_t firmwareMajor;
     uint8_t firmwareMinor;
     bool firmwareDebug;
@@ -110,7 +83,6 @@ void setup() {
     uint8_t hardwareMinor;
     uint8_t protocolMajor;
     uint8_t protocolMinor;
-
     error = svm41.getVersion(firmwareMajor, firmwareMinor, firmwareDebug,
                              hardwareMajor, hardwareMinor, protocolMajor,
                              protocolMinor);
@@ -120,32 +92,26 @@ void setup() {
         errorToString(error, errorMessage, 256);
         Serial.println(errorMessage);
     } else {
-        Serial.print("FirmwareMajor:");
+        Serial.print("Firmware version: ");
         Serial.print(firmwareMajor);
-        Serial.print("\t");
-        Serial.print("FirmwareMinor:");
+        Serial.print(".");
         Serial.print(firmwareMinor);
         Serial.print("\t");
-        Serial.print("FirmwareDebug:");
-        Serial.print(firmwareDebug);
-        Serial.print("\t");
-        Serial.print("HardwareMajor:");
+        Serial.print("FirmwareDebug: ");
+        Serial.println(firmwareDebug);
+        Serial.print("Hardware version: ");
         Serial.print(hardwareMajor);
-        Serial.print("\t");
-        Serial.print("HardwareMinor:");
-        Serial.print(hardwareMinor);
-        Serial.print("\t");
-        Serial.print("ProtocolMajor:");
+        Serial.print(".");
+        Serial.println(hardwareMinor);
+        Serial.print("Protocol version: ");
         Serial.print(protocolMajor);
-        Serial.print("\t");
-        Serial.print("ProtocolMinor:");
-        Serial.println(protocolMinor);
+        Serial.print(".");
+        Serial.print(protocolMinor);
+        Serial.println();
     }
 
     // Start Measurement
-
     error = svm41.startMeasurement();
-
     if (error) {
         Serial.print("Error trying to execute startMeasurement(): ");
         errorToString(error, errorMessage, 256);
@@ -157,9 +123,28 @@ void loop() {
     uint16_t error;
     char errorMessage[256];
 
-    // TODO: DRIVER_GENERATOR Adjust to correct measurement delay
-    delay(1000);
     // Read Measurement
-    // TODO: DRIVER_GENERATOR Add scaling and offset to printed measurement
-    // values
+    delay(1000);
+    float humidity;
+    float temperature;
+    float vocIndex;
+    float noxIndex;
+    error = svm41.readMeasuredValues(humidity, temperature, vocIndex, noxIndex);
+    if (error) {
+        Serial.print("Error trying to execute readMeasuredValues(): ");
+        errorToString(error, errorMessage, 256);
+        Serial.println(errorMessage);
+    } else {
+        Serial.print("Humidity:");
+        Serial.print(humidity);
+        Serial.print("\t");
+        Serial.print("Temperature:");
+        Serial.print(temperature);
+        Serial.print("\t");
+        Serial.print("VocIndex:");
+        Serial.print(vocIndex);
+        Serial.print("\t");
+        Serial.print("NoxIndex:");
+        Serial.println(noxIndex);
+    }
 }
